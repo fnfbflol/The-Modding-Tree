03c2f8751596a59e5ec56f1c492bfd5f6c399cdf
fnfbflol <aredinananthonio@gmail.com>
2025-12-04 20:49:13 +0000

    messing around lel

--- js/layers.js
+++ js/layers.js
@@ -6,13 +6,13 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    color: "#ffe100ff",
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.9, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
