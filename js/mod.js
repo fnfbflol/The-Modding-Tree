cacc3407d3dd42e6870ff7836eb4247e2d0ae469
fnfbflol <aredinananthonio@gmail.com>
2025-12-05 07:04:42 +0000

    idk

--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "Beginning",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -55,7 +55,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("e930293"))
 }
 
 
